6ec91957f2ae6f2c0fc763571cc8b67b591c4af6
Ivann <ivan.linzov@gmail.com>
2022-07-05 17:52:03 +0300

    added app.py, starting using flask

---
+++ DBhandler/DBhandler.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+class DBHandler():
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    # Получить все элементы из таблицы
+    def getItems(self, table):
+        try:
+            self.__cur.execute(f'SELECT * from {table};')
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as e:
+            print('Ошибка чтения из БД', e)
+        return []
+
+    # Добавить элемент в таблицу
+    def addItem(self, table, args):
+        items = ','.join(["'" + str(field) + "'" for field in args])
+        print(items)
+        try:
+            self.__cur.execute(f"INSERT INTO {table} VALUES ({items});")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка добавления записи в БД', e)
+            return False
+        return True
+
+    # Получить элемент в таблице по ID
+    def getItemById(self, table, id):
+        try:
+            self.__cur.execute(f"SELECT * FROM {table} WHERE ID = {id} LIMIT 1;")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print('Ошибка чтения записи из БД', e)
+        return False
+
+    # Удалить элемент по ID
+    def deleteItemById(self, table, id):
+        try:
+            self.__cur.execute(f"DELETE FROM {table} WHERE ID = {id} LIMIT 1;")
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print('Ошибка удаления записи из БД', e)
+            return False
+        return True
+
+    # Получить элемент по значению поля
+    def getItemByField(self, table, field, value):
+        try:
+            self.__cur.execute(f"SELECT * FROM {table} WHERE {field} = '{value}' LIMIT 1;")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print('Ошибка чтения записи из БД', e)
+        return False
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, g, render_template
+import sqlite3
+from DBhandler.DBhandler import DBHandler
+import os
+
+
+app = Flask(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'database.db')))
+
+
+controller = None
+
+@app.before_request
+def before_request():
+    #Устанавливаем соединение с БД
+    global controller
+    controller = DBHandler(get_db())
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    # Вспомогательная функция для создания таблиц БД
+    db = connect_db()
+    with app.open_resource('games.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    #Соединение с БД, если оно еще не установлено
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.teardown_appcontext
+def close_db(error):
+    #Закрываем соединение с БД, если оно было установлено
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+#базовая страница
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+#Точка входа
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.run(debug=True)
