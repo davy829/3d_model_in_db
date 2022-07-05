b28720f1857c3385ec4fc413da72b9c9401731ea
Ivann <ivan.linzov@gmail.com>
2022-07-05 17:42:31 +0300

    added dbhandler, all crud operations realized

--- models/BaseModel.py
+++ models/BaseModel.py
@@ -1,5 +1,4 @@
-from msilib.schema import Control
-from controllers.BaseController import Controller
+from DBhandler.DBhandler import DBHandler
 
 #Базовый класс модели
 class BaseModel():
@@ -8,21 +7,21 @@ class BaseModel():
 
     # Получить все записи для модели
     @staticmethod
-    def getAll(table: str, controller: Controller) -> list:
+    def getAll(table: str, controller: DBHandler) -> list:
         return controller.getItems(table)
 
     # Получить элемент модели по ID
     @staticmethod
-    def getItemByID(table: str, id: int, controller: Controller) -> tuple:
+    def getItemByID(table: str, id: int, controller: DBHandler) -> tuple:
         return controller.getItemById(table, id)
 
     # Удалить элемент модели по ID
     @staticmethod
-    def deleteItemByID(table: str, id: int, controller: Controller) -> bool:
+    def deleteItemByID(table: str, id: int, controller: DBHandler) -> bool:
         return controller.deleteItemById(table, id)
 
     # Добавить элемент 
-    def addItem(self, controller: Controller):
+    def addItem(self, controller: DBHandler):
         return controller.addItem(self.table, self.getAllFields())
 
     # Получить все поля объекта для его сериализации
